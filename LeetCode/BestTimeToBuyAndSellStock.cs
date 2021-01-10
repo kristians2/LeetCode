07602db555349f56c34a8149b19947ddc2bcfa5e
Kristians <you@example.com>
2021-01-10 19:32:14 +0200

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ LeetCode/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int min = int.MaxValue;
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                } else
+                {
+                    int diff = prices[i] - min;
+
+                    if (maxProfit < diff)
+                    {
+                        maxProfit = diff;
+                    }
+                }
+    
+
+            }
+
+            return maxProfit;
+        }
+    }
+}
