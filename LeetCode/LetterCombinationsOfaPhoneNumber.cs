94a1df680e0f6c9f25a1611f832bc9fea78c659a
Kristians <you@example.com>
2021-01-12 12:17:56 +0200

    https://leetcode.com/problems/letter-combinations-of-a-phone-number/

---
+++ LeetCode/LetterCombinationsOfaPhoneNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LetterCombinationsOfaPhoneNumber
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            List<string> list = new List<string>();
+
+            string[] mappings = new string[] {
+                "",
+                "",
+                "abc",
+                "def",
+                "ghi",
+                "jkl",
+                "mno",
+                "pqrs",
+                "tuv",
+                "wxyz",
+            };
+
+            char[] d = digits.ToCharArray();
+
+            BuildString(mappings, list, d, 0, "");
+
+            return list;
+        }
+
+        public void BuildString(string[] mappings, List<string> list, char[] digits, int depth, string str)
+        {
+           if (depth == digits.Length)
+           {
+                list.Add(str);
+                return;
+           }
+
+            char[] current = mappings[(int) Char.GetNumericValue(digits[depth])].ToCharArray();
+            
+            for (int i = 0; i < current.Length; i++)
+            {
+                BuildString(mappings, list, digits, depth + 1, str + current[i]);
+            }
+        }
+    }
+}
