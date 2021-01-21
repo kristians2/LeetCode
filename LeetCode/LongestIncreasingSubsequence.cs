6cf35044aec9f008cf1e0eb63ac34f96eac5d038
Kristians <kristian.savicki@gmail.com>
2021-01-21 11:13:31 +0200

    https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ LeetCode/LongestIncreasingSubsequence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class LongestIncreasingSubsequence
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            int[] dp = new int[nums.Length];
+            Array.Fill<int>(dp, 1);
+
+            for (int i = 1; i < dp.Length; i++)
+            {
+                int max = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        if (dp[j] > max)
+                        {
+                            max = dp[j];
+                        }
+                    }
+                }
+
+                dp[i] = max + 1;
+            }
+
+            return dp.Max();
+        }
+    }
+}
