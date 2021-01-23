636d72c2cb009d75e4a49b22cb870eb6c981a528
Kristians <kristian.savicki@gmail.com>
2021-01-23 21:44:20 +0200

    https://leetcode.com/problems/jump-game

---
+++ LeetCode/JumpGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return true;
+            }
+
+            if (nums[0] == 0) {
+                return false;
+            }
+
+
+            int[] dp = new int[nums.Length];
+            Array.Fill<int>(dp, 0);
+
+            dp[0] = 1;
+
+            for (int i = 0; i < dp.Length - 1; i++)
+            {
+                for (int j = i + 1; j <= i + nums[i]; j++)
+                {
+                    if (j > dp.Length - 1)
+                    {
+                        dp[dp.Length - 1] = 1;
+                        break;
+                    }
+
+                    dp[j] = 1;
+                }
+            }
+
+            return !dp.Contains<int>(0);
+        }
+    }
+}
