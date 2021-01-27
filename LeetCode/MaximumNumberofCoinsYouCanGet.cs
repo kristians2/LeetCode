fb87c58a64dc25ef790a426204ad3b9c2a49e5c2
Kristians <kristian.savicki@gmail.com>
2021-01-27 12:54:41 +0200

    https://leetcode.com/problems/maximum-number-of-coins-you-can-get/

---
+++ LeetCode/MaximumNumberofCoinsYouCanGet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MaximumNumberofCoinsYouCanGet
+    {
+        public int MaxCoins(int[] piles) {
+            int result = 0;
+            int k = piles.Length / 3;
+            
+            Array.Sort<int>(piles);
+
+            for (int i = piles.Length - 2; i > k - 1; i -= 2)
+            {
+                result += piles[i];
+            }
+
+            return result;
+        }
+    }
+}
